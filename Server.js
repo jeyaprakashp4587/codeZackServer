7fba0c862d67a66f51f2c19fc9f3e7483d70bdea
Jeya Prakash <126503394+jeyaprakashp4587@users.noreply.github.com>
2025-04-21 07:56:27 +0530

    Update Server.js

--- Server.js
+++ Server.js
@@ -33,7 +33,9 @@ DB1.on("connected", () => {
 DB2.on("connected", () => {
   console.log("DB2 is connected");
 });
-app.get("/",(req,res) => res.send("server is alive"))
+app.get("/",(req,res) => {
+  res.send("server is alive")
+})
 // Routers
 app.use("/LogIn", LogIn);
 app.use("/Courses", Course);
